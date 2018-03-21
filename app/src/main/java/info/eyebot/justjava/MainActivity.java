9e072d141b445790502a5cf5ea4d2be95a95243f
Myron Zhang <myron.z.zhang@gmail.com>
2018-03-21 23:42:52 +0800

    Finish lecture 2B

--- app/src/main/java/info/eyebot/justjava/MainActivity.java
+++ app/src/main/java/info/eyebot/justjava/MainActivity.java
@@ -30,7 +30,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity * 5);
+        int quantity = 55;
+        String priceMessage = "Total: $" + quantity;
+        priceMessage = priceMessage + "\nThank you!";
+        displayMessage(priceMessage);
     }
 
     public void increment(View view) {
@@ -57,5 +60,13 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
