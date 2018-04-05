0f5b17dc22d463ee2a4d7fe1d952bea839c6d9b6
Myron Zhang <myron.z.zhang@gmail.com>
2018-04-05 16:56:10 +0800

    Finish lecture User Input

--- app/src/main/java/info/eyebot/justjava/MainActivity.java
+++ app/src/main/java/info/eyebot/justjava/MainActivity.java
@@ -6,10 +6,15 @@
  */
 package info.eyebot.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -19,6 +24,8 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 2;
+    boolean hasWhippedCream = false;
+    boolean hasChocolate = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +37,53 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 55;
-        String priceMessage = "Total: $" + quantity;
+        CheckBox whippedcreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        hasWhippedCream = whippedcreamCheckBox.isChecked();
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        hasChocolate = chocolateCheckBox.isChecked();
+        EditText customerNameEditText = (EditText) findViewById(R.id.customer_name_edittext);
+        String customerName = customerNameEditText.getText().toString();
+
+        int pricePerCup = 5;
+        if (hasWhippedCream) {
+            pricePerCup = pricePerCup + 1;
+        }
+        if (hasChocolate) {
+            pricePerCup = pricePerCup + 2;
+        }
+
+        String priceMessage = "Name: " + customerName;
+        priceMessage = priceMessage + "\nAdd whipped cream? " + hasWhippedCream;
+        priceMessage = priceMessage + "\nAdd Chocolate? " + hasChocolate;
+        priceMessage = priceMessage + "\nQuantity: " + quantity;
+        priceMessage = priceMessage + "\nTotal: $" + quantity * pricePerCup;
         priceMessage = priceMessage + "\nThank you!";
-        displayMessage(priceMessage);
+        //displayMessage(priceMessage);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, "myron.z.zhang@gmail.com");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "咖啡订单");
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     public void increment(View view) {
+        if (quantity >= 100) {
+            Toast.makeText(this, "数量不能大于100", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity + 1;
         display(quantity);
     }
 
     public void decrement(View view) {
+        if (quantity <= 1) {
+            Toast.makeText(this, "数量不能小于1", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity - 1;
         display(quantity);
     }
